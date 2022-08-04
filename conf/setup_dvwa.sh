8deeddbb53c052fbd3213c5839c6ae7c36ca97cd
zeeshanbhattined <110241653+zeeshanbhattined@users.noreply.github.com>
2022-08-04 20:45:31 +0500

    Update setup_dvwa.sh

--- conf/setup_dvwa.sh
+++ conf/setup_dvwa.sh
@@ -8,3 +8,4 @@ sed -i 's/FileInfo/All/g' /etc/apache2/sites-available/000-default.conf && \
 sed -i 's/root/admin/g' /app/config/config.inc.php && \
 echo "sed -i \"s/p@ssw0rd/\$PASS/g\" /app/config/config.inc.php" >> /create_mysql_admin_user.sh && \
 echo 'session.save_path = "/tmp"' >> /etc/php5/apache2/php.ini
+
